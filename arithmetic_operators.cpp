643fc5f87e31fb1651fe27dc8c51b60dc6cd28a1
ravikrishna-undavalli <ravikrishna2349@gmail.com>
2025-05-05 04:55:58 +0000

    Arithmetic operators examples

---
+++ arithmetic_operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 25, num2 = 3;
+    cout<<"Addittion:"<<num1+num2<<endl;
+    cout<<"Subtraction:"<<num1-num2<<endl;
+    cout<<"Multiplication:"<<num1*num2<<endl;
+    cout<<"Division:"<<num1/num2<<endl;
+    cout<<"Modulus:"<<num1%num2<<endl;
+}
