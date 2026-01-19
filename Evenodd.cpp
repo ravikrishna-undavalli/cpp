dc7c0a0ad09574ea7217cc79090cf356032bccec
Undavalli Naga Ravi Krishna <ravikrishna2349@gmail.com>
2026-01-19 22:28:43 +0530

    Create Evenodd.cpp

---
+++ Evenodd.cpp
@@ -0,0 +1,20 @@
+// Logic to find given number is Even or odd
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num=0;
+    cout<<"Enter the number:";
+    cin>>num;
+    if(num%2==0)
+    {
+      cout<<"Even number"<<endl;
+    }
+    else
+    {
+       cout<<"Odd number"<<endl;
+    }
+}
